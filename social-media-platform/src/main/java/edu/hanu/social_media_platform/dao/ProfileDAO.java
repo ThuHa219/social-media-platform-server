671b8f37ad00b3fd282e4708df51a7cb78b95520
quang <quang@192.168.135.100>
2020-05-29 17:59:13 +0700

    Add authentication for password in save() and update()

--- social-media-platform/src/main/java/edu/hanu/social_media_platform/dao/ProfileDAO.java
+++ social-media-platform/src/main/java/edu/hanu/social_media_platform/dao/ProfileDAO.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,15 +14,18 @@ import javax.ws.rs.core.MediaType;
 
 import edu.hanu.social_media_platform.model.Profile;
 import edu.hanu.social_media_platform.utils.DbUtils;
+import edu.hanu.social_media_platform.utils.PasswordAuthentication;
 
-public class ProfileDAO implements DAO<Profile>{
+public class ProfileDAO implements DAO<Profile> {
 	private static final String INSERT_SQL_QUERY = "INSERT INTO profile(firstname, lastname, time_created, profilename, email, phoneNumber, address, password, answer, question) VALUES(?, ?, now(), ?, ?, ?, ?, ?, ?, ?)";
 	private static final String UPDATE_SQL_QUERY = "UPDATE profile SET firstname = ?," + "	lastname = ?,"
-			+ "	profilename = ?," + " email = ?," + " phoneNumber = ?," + " address = ?," + "answer = ?, " + "question = ?, " + " password = ? WHERE profile.profilename = ?";
+			+ "	profilename = ?," + " email = ?," + " phoneNumber = ?," + " address = ?," + "answer = ?, "
+			+ "question = ?, " + " password = ? WHERE profile.profilename = ?";
 	private static final String SELECT_SQL_QUERY = "SELECT * FROM profile WHERE profile.profilename = ?";
 	private static final String SELECT_ALL_SQL_QUERY = "SELECT * FROM profile";
 	private static final String DELETE_SQL_QUERY = "DELETE FROM profile WHERE profile.profilename = ?";
 	private static final String DELETE_ALL_SQL_QUERY = "DELETE FROM profile";
+	PasswordAuthentication authentication = new PasswordAuthentication();
 
 	@Override
 	public List<Profile> getAll() {
@@ -68,6 +70,7 @@ public class ProfileDAO implements DAO<Profile>{
 		return profiles;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public long save(Profile p) {
 		Connection conn = null;
@@ -85,10 +88,10 @@ public class ProfileDAO implements DAO<Profile>{
 			ps.setString(4, p.getEmail());
 			ps.setString(5, p.getPhoneNumber());
 			ps.setString(6, p.getAddress());
-			ps.setString(7, p.getPassword());
+			ps.setString(7, authentication.hash(p.getPassword()));
 			ps.setString(8, p.getAnswer());
 			ps.setString(9, p.getQuestion());
-			
+
 			ps.execute();
 			System.out.println(ps.toString());
 			conn.commit();
@@ -114,6 +117,7 @@ public class ProfileDAO implements DAO<Profile>{
 		return 1;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void update(Profile p) {
 		Connection conn = null;
@@ -132,7 +136,7 @@ public class ProfileDAO implements DAO<Profile>{
 			ps.setString(4, p.getEmail());
 			ps.setString(5, p.getPhoneNumber());
 			ps.setString(6, p.getAddress());
-			ps.setString(7, p.getPassword());
+			ps.setString(7, authentication.hash(p.getPassword()));
 			ps.setString(8, p.getAnswer());
 			ps.setString(9, p.getQuestion());
 			ps.setString(10, p.getProfileName());
@@ -181,7 +185,7 @@ public class ProfileDAO implements DAO<Profile>{
 			}
 		}
 	}
-	
+
 	public Profile get(String profilename) {
 		Connection conn = null;
 		PreparedStatement ps = null;
@@ -219,9 +223,9 @@ public class ProfileDAO implements DAO<Profile>{
 				e.printStackTrace();
 			}
 		}
-		return profile;	
+		return profile;
 	}
-	
+
 	public void delete(String profilename) {
 		Connection conn = null;
 		PreparedStatement ps = null;
@@ -245,6 +249,7 @@ public class ProfileDAO implements DAO<Profile>{
 			}
 		}
 	}
+
 	public static void main(String[] args) {
 //		Profile p = new Profile();
 //		p.setFirstName("Ha");
@@ -257,12 +262,12 @@ public class ProfileDAO implements DAO<Profile>{
 //		dao.save(p);
 //		
 		System.out.println(dao.get("ThuHa219").toString());
-		
+
 		Client client = ClientBuilder.newClient();
 		final WebTarget baseTarget = client.target("http://localhost:8080/social-media-platform-server/webapi");
 		WebTarget resourceTarget = baseTarget.path("/{resourceName}");
 		WebTarget resourceTargetId = resourceTarget.path("/{resourceId}");
-		
+
 		System.out.println("hello");
 		Profile profile = resourceTargetId.resolveTemplate("resourceName", "profiles")
 				.resolveTemplate("resourceId", "ThuHa219").request(MediaType.APPLICATION_JSON).get(Profile.class);
@@ -279,6 +284,6 @@ public class ProfileDAO implements DAO<Profile>{
 
 	@Override
 	public void delete(long id) {
-		//do nothing
+		// do nothing
 	}
 }
